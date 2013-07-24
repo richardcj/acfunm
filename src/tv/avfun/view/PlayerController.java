a56f657e538788514752d02d85d98ed4011eb4bf
Yrom <yrom@qq.com>
2013-07-25 07:58:29 +0800

    new player controller

---
+++ src/tv/avfun/view/PlayerController.java
@@ -0,0 +1,13 @@
+package tv.avfun.view;
+
+import android.content.Context;
+import io.vov.vitamio.widget.MediaController;
+
+
+public class PlayerController extends MediaController {
+
+    public PlayerController(Context context) {
+        super(context);
+    }
+
+}
